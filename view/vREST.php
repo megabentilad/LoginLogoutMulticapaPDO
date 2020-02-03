2f8ceb9301d5ad2a234efdae5eb03222e0fc18ba
luism <luism.rivuri@educa.jcyl.es>
2020-02-03 13:51:42 +0100

    cosas de pokemones creo

--- view/vREST.php
+++ view/vREST.php
@@ -1,7 +1,7 @@
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <fieldset>
         <legend>Pokeapi</legend>
-        <label for="pokemon">ID o nombre del pokemon: </label>
+        <label for="pokemon" class="labelbuscador">ID o nombre del pokemon: </label>
         <input type="text" id="pokemon" name="pokemon" placeholder="azumarill" value="<?php echo $_SESSION['DAW215LLBusquedaPokemon']; ?>">
         <br/>
         <div class="shiny-box">
@@ -32,6 +32,7 @@
     <?php } ?>
 <a href="<?php echo $_SERVER['PHP_SELF'] . "?pagina=inicio"; ?>"><input type="button" name="volver" value="Volver" id="volver"></a>
 <img src="<?php if(isset($_SESSION['DAW215LLBusquedaPokemonSprite'])){ echo $_SESSION['DAW215LLBusquedaPokemonSprite']; } ?>" class="pokemonSprite">
+<br/>
 <img src="<?php if(isset($_SESSION['DAW215LLBusquedaPokemonSprite2'])){ echo $_SESSION['DAW215LLBusquedaPokemonSprite2']; } ?>" class="pokemonSprite2">
 <?php if(isset($_SESSION['DAW215LLBusquedaPokemonDatos'])){ ?>
 <div class="pokedatos">
